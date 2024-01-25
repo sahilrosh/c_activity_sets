e2a82a0c79ff95127cc8776e1fb938ca20685b59
Sahil Roshan <sahil@Sahils-MacBook-Air.local>
2024-01-25 10:15:47 +0530

    viva

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,10 @@
 //2. Write a C program to add two numbers.
+#include<stdio.h>
 
+int main()
+{
+    int a,b,sum;
+    sum=a+b;
+    printf("the sum of a and b is %d",sum);
+    return 0;
+}
---
+++ set01/problem03.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int input();
+int add(int a, int b);
+void output(int a, int b, int sum);
+
+int main()
+{
+    int a,b,sum;
+    a=input();
+    b=input();
+    sum=add(a,b);
+    output(a,b,sum);
+}
+int input()
+{
+    int x;
+    printf("enter the value of x");
+    scanf("%d",x);
+    return x;
+}
+
+int add(int a, int b)
+{
+    int a,b,sum;
+    sum=a+b;
+    return sum;
+}
+
+void output(int a, int b, int sum)
+{
+    printf("the sum od %d and %d is %d",a,b,sum);
+}
